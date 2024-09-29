4f8b4d8ac5f85a062041b907c12661226f02766a
prajapatibhaskar <141663471+prajapatibhaskar@users.noreply.github.com>
2024-09-29 18:44:22 +0530

    Initial flask-app structure.

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pickle
+from tensorflow.keras.models import load_model
+
+app = Flask(__name__)
+CORS(app)
+
+# Load the model and tokenizer
+with open('resources/tfidf_tokenizer.pkl', 'rb') as f:
+    tfidf_tokenizer = pickle.load(f)
+loaded_model = load_model('resources/text_classification_model.h5')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    text = data.get('text')
+    
+    if not text or not text.strip():
+        return jsonify({'error': 'No text provided'}), 400
+    
+    text_features = tfidf_tokenizer.transform([text])
+    predictions = loaded_model.predict(text_features)
+    predicted_label = int(predictions[0][0])
+    
+    return jsonify({'predicted_label': predicted_label})
+
+if __name__ == '__main__':
+    app.run(debug=True)
